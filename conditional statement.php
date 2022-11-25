dbdfdaac72121e3fcd88b7c9dbfec93eac4e8ec4
Nikesh Raj <rajnikesh688@gmail.com>
2022-11-25 10:15:44 +0545

    php

---
+++ conditional statement.php
@@ -0,0 +1,12 @@
+<?php
+$marks=50;
+if ($marks>100){
+    echo "Distinction";
+}
+elseif ($marks>70 && $marks<80){
+    echo "First division";
+}
+else{
+    echo "pass";
+}
+?>
---
+++ forloop-60.php
@@ -0,0 +1,9 @@
+<?php 
+for ($x = 2; $x <= 100; $x+=2) {
+
+if ($x==60){
+continue;
+        }
+    echo "The number is: $x <br>";
+} 
+?>
