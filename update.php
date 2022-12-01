afb40ab2684bcfb304a45e33eed17911a2189f85
Nikesh Raj <rajnikesh688@gmail.com>
2022-12-01 09:37:38 +0545

    phplogin

---
+++ update.php
@@ -0,0 +1,13 @@
+<?php
+include 'db.php';
+
+$sql = "UPDATE myguest SET lastname='Smith' WHERE id=2";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Record updated successfully";
+} else {
+  echo "Error updating record: " . $conn->error;
+}
+
+$conn->close();
+?>
